c1266d6a50ef179e636be0c351913103de736778
Chris <chris.brewin@canstar.com.au>
2023-05-19 15:10:10 +1000

    remove sync from copy function

--- cloner-go/pkg/copy_file.go
+++ cloner-go/pkg/copy_file.go
@@ -7,20 +7,16 @@ import (
 )
 
 func CopyFile(source string, destination string) error {
-	// fmt.Println("Copying: ", source, " to ", destination)
-	// open the source file
 	sourceFile, err := os.Open(source)
 	if err != nil {
 		return fmt.Errorf("CopyFile: %w", err)
 	}
-	// close the source file when the function completes execution
 	defer sourceFile.Close()
-	// create a new file in the destination
+
 	destinationFile, err := os.Create(destination)
 	if err != nil {
 		return fmt.Errorf("CopyFile: %w", err)
 	}
-	// close the destination file when the function completes execution
 	defer func() {
 		cerr := destinationFile.Close()
 		if err == nil {
@@ -28,18 +24,12 @@ func CopyFile(source string, destination string) error {
 		}
 	}()
 
-	// copy the content of the source file to the destination file
 	_, err = io.Copy(destinationFile, sourceFile)
 	if err != nil {
 		return fmt.Errorf("CopyFile: %w", err)
 	}
-	// set the file permission mode
-	err = os.Chmod(destination, 0777)
-	if err != nil {
-		return fmt.Errorf("CopyFile: %w", err)
-	}
-	// copy the file permissions
-	err = destinationFile.Sync()
+
+	err = destinationFile.Chmod(0777)
 	if err != nil {
 		return fmt.Errorf("CopyFile: %w", err)
 	}
